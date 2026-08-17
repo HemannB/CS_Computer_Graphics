deeb3f4ea68a3b8776dbd1c32b3bbe4bfab65260
HemannB <brunohemann@gmail.com>
2026-08-16 21:11:24 -0300

    feat: add multi-axis transformation controls to cube
    - add directional translation on the X, Y, and Z axes
    - add continuous 360-degree rotation on all axes
    - add independent scaling with minimum and maximum limits
    - support orthographic and perspective projections
    - reset all transformations with the Space key
    - preserve the existing WIP cube geometry
    
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- Lesson04_August_12/cube.cpp
+++ Lesson04_August_12/cube.cpp
@@ -3,7 +3,7 @@
  * File: cube.cpp
  * Author: Bruno Hemann
  * Date: 12/08/2026
- * Description:
+ * Description: Cubo 3D com projeções, translação, rotação e escala.
  * *********************************************************************
  */
 
@@ -13,6 +13,9 @@
 
 #include <iostream>
 
+const int kWindowWidth = 800;
+const int kWindowHeight = 800;
+
 bool orthographic_view = true;
 const GLfloat kOrthographicLimitX = 5;
 const GLfloat kOrthographicLimitY = 5;
@@ -20,27 +23,49 @@ const GLfloat kOrthographicLimitZ = 20;
 
 bool perspective_view = false;
 const GLfloat kPerspectiveFieldOfViewAngle = 45;
-const GLfloat kPerspectiveNearZ = 0.1;
+const GLfloat kPerspectiveNearZ = 0.1f;
 const GLfloat kPerspectiveFarZ = 100;
 const GLfloat kPerspectiveTranslateZ = 10;
 
-bool translating = false;
-const GLfloat kTranslateLimit = 10;
+bool translatingL = false;
+bool translatingR = false;
+bool translatingU = false;
+bool translatingD = false;
+bool translatingNear = false;
+bool translatingFar = false;
+const GLfloat kTranslateLimit = 3;
 const GLfloat kDefaultTranslate = 0;
-GLfloat translate_increment = 0.1;
+GLfloat translate_increment = 0.1f;
 GLfloat translate = kDefaultTranslate;
+GLfloat translateY = kDefaultTranslate;
+GLfloat translateZ = kDefaultTranslate;
 
-bool rotating = false;
-const GLfloat kRotateAngleLimit = 180;
+bool rotatingU = false;
+bool rotatingD = false;
+bool rotatingL = false;
+bool rotatingR = false;
+bool rotatingAntiClockwise = false;
+bool rotatingClockwise = false;
+const GLfloat kRotateAngleLimit = 360;
 const GLfloat kDefaultRotateAngle = 0;
-GLfloat rotate_angle_increment = 1;
+GLfloat rotate_angle_increment = 1.0f;
 GLfloat rotate_angle = kDefaultRotateAngle;
+GLfloat rotate_angleY = kDefaultRotateAngle;
+GLfloat rotate_angleZ = kDefaultRotateAngle;
 
-bool scaling = false;
-const GLfloat kScaleLimit = 10;
-const GLfloat kDefaultScale = 1;
-GLfloat scale_increment = 0.1;
-GLfloat scale = kDefaultScale;
+bool scalingL = false;
+bool scalingR = false;
+bool scalingU = false;
+bool scalingD = false;
+bool scalingNear = false;
+bool scalingFar = false;
+const GLfloat kScaleMin = 0.1f;
+const GLfloat kScaleMax = 10.0f;
+const GLfloat kDefaultScale = 1.0f;
+GLfloat scale_increment = 0.1f;
+GLfloat scaleX = kDefaultScale;
+GLfloat scaleY = kDefaultScale;
+GLfloat scaleZ = kDefaultScale;
 
 void keyboard_read(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -49,8 +74,14 @@ void keyboard_read(GLFWwindow *window) {
 
   if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
     translate = kDefaultTranslate;
+    translateY = kDefaultTranslate;
+    translateZ = kDefaultTranslate;
     rotate_angle = kDefaultRotateAngle;
-    scale = kDefaultScale;
+    rotate_angleY = kDefaultRotateAngle;
+    rotate_angleZ = kDefaultRotateAngle;
+    scaleX = kDefaultScale;
+    scaleY = kDefaultScale;
+    scaleZ = kDefaultScale;
   }
 
   if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) {
@@ -63,26 +94,71 @@ void keyboard_read(GLFWwindow *window) {
     perspective_view = true;
   }
 
-  if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS) {
-    translating = true;
-  }
-  if (glfwGetKey(window, GLFW_KEY_T) == GLFW_RELEASE) {
-    translating = false;
-  }
+  bool translateLeftPressed = (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS &&
+                               glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS);
+  bool translateRightPressed =
+      (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS &&
+       glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS);
+  bool translateUpPressed = (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS &&
+                             glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS);
+  bool translateDownPressed = (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS &&
+                               glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS);
+  bool translateNearPressed =
+      (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS &&
+       glfwGetKey(window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS);
+  bool translateFarPressed =
+      (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS &&
+       glfwGetKey(window, GLFW_KEY_PAGE_UP) == GLFW_PRESS);
 
-  if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
-    rotating = true;
-  }
-  if (glfwGetKey(window, GLFW_KEY_R) == GLFW_RELEASE) {
-    rotating = false;
-  }
+  translatingL = translateLeftPressed;
+  translatingR = translateRightPressed;
+  translatingU = translateUpPressed;
+  translatingD = translateDownPressed;
+  translatingNear = translateNearPressed;
+  translatingFar = translateFarPressed;
 
-  if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
-    scaling = true;
-  }
-  if (glfwGetKey(window, GLFW_KEY_E) == GLFW_RELEASE) {
-    scaling = false;
-  }
+  bool rotateUpPressed = (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS &&
+                          glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS);
+  bool rotateDownPressed = (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS &&
+                            glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS);
+  bool rotateLeftPressed = (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS &&
+                            glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS);
+  bool rotateRightPressed = (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS &&
+                             glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS);
+  bool rotateAntiClockwisePressed =
+      (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS &&
+       glfwGetKey(window, GLFW_KEY_PAGE_UP) == GLFW_PRESS);
+  bool rotateClockwisePressed =
+      (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS &&
+       glfwGetKey(window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS);
+
+  rotatingU = rotateUpPressed;
+  rotatingD = rotateDownPressed;
+  rotatingL = rotateLeftPressed;
+  rotatingR = rotateRightPressed;
+  rotatingAntiClockwise = rotateAntiClockwisePressed;
+  rotatingClockwise = rotateClockwisePressed;
+
+  bool scaleLeftPressed = (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS &&
+                           glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS);
+  bool scaleRightPressed = (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS &&
+                            glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS);
+  bool scaleUpPressed = (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS &&
+                         glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS);
+  bool scaleDownPressed = (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS &&
+                           glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS);
+  bool scaleNearPressed =
+      (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS &&
+       glfwGetKey(window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS);
+  bool scaleFarPressed = (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS &&
+                          glfwGetKey(window, GLFW_KEY_PAGE_UP) == GLFW_PRESS);
+
+  scalingL = scaleLeftPressed;
+  scalingR = scaleRightPressed;
+  scalingU = scaleUpPressed;
+  scalingD = scaleDownPressed;
+  scalingNear = scaleNearPressed;
+  scalingFar = scaleFarPressed;
 }
 
 void resize_window(GLFWwindow *window) {
@@ -95,7 +171,7 @@ void resize_window(GLFWwindow *window) {
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   if (orthographic_view) {
-    glfwSetWindowTitle(window, "Tetraedro ortogonal");
+    glfwSetWindowTitle(window, "Cubo ortogonal");
     GLdouble left = -kOrthographicLimitX;
     GLdouble right = kOrthographicLimitX;
     GLdouble bottom = -kOrthographicLimitY;
@@ -111,7 +187,7 @@ void resize_window(GLFWwindow *window) {
     }
   }
   if (perspective_view) {
-    glfwSetWindowTitle(window, "Tetraedro em perspectiva");
+    glfwSetWindowTitle(window, "Cubo em perspectiva");
     GLdouble field_of_view = kPerspectiveFieldOfViewAngle;
     GLdouble near = kPerspectiveNearZ;
     GLdouble far = kPerspectiveFarZ;
@@ -123,28 +199,124 @@ void draw() {
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 
-  glTranslatef(translate, 0, -kPerspectiveTranslateZ);
-  if (translating) {
+  GLfloat perspectiveTranslateZ =
+      perspective_view ? -kPerspectiveTranslateZ : 0.0f;
+  glTranslatef(translate, translateY, perspectiveTranslateZ + translateZ);
+
+  if (translatingL) {
+    translate -= translate_increment;
+    if (translate < -kTranslateLimit) {
+      translate = -kTranslateLimit;
+    }
+  }
+  if (translatingR) {
     translate += translate_increment;
-    if ((translate <= -kTranslateLimit) || (translate >= kTranslateLimit)) {
-      translate_increment *= -1;
+    if (translate > kTranslateLimit) {
+      translate = kTranslateLimit;
+    }
+  }
+  if (translatingU) {
+    translateY += translate_increment;
+    if (translateY > kTranslateLimit) {
+      translateY = kTranslateLimit;
+    }
+  }
+  if (translatingD) {
+    translateY -= translate_increment;
+    if (translateY < -kTranslateLimit) {
+      translateY = -kTranslateLimit;
+    }
+  }
+  if (translatingNear) {
+    translateZ += translate_increment;
+    if (translateZ > kTranslateLimit) {
+      translateZ = kTranslateLimit;
+    }
+  }
+  if (translatingFar) {
+    translateZ -= translate_increment;
+    if (translateZ < -kTranslateLimit) {
+      translateZ = -kTranslateLimit;
     }
   }
 
   glRotatef(rotate_angle, 1, 0, 0);
-  if (rotating) {
+  if (rotatingU) {
     rotate_angle += rotate_angle_increment;
-    if ((rotate_angle <= -kRotateAngleLimit) ||
-        (rotate_angle >= kRotateAngleLimit)) {
-      rotate_angle_increment *= -1;
+    if (rotate_angle >= kRotateAngleLimit) {
+      rotate_angle = kDefaultRotateAngle;
+    }
+  }
+  if (rotatingD) {
+    rotate_angle -= rotate_angle_increment;
+    if (rotate_angle <= -kRotateAngleLimit) {
+      rotate_angle = kDefaultRotateAngle;
     }
   }
 
-  glScalef(scale, 1, 1);
-  if (scaling) {
-    scale += scale_increment;
-    if ((scale <= -kScaleLimit) || (scale >= kScaleLimit)) {
-      scale_increment *= -1;
+  glRotatef(rotate_angleY, 0, 1, 0);
+  if (rotatingL) {
+    rotate_angleY += rotate_angle_increment;
+    if (rotate_angleY >= kRotateAngleLimit) {
+      rotate_angleY = kDefaultRotateAngle;
+    }
+  }
+  if (rotatingR) {
+    rotate_angleY -= rotate_angle_increment;
+    if (rotate_angleY <= -kRotateAngleLimit) {
+      rotate_angleY = kDefaultRotateAngle;
+    }
+  }
+
+  glRotatef(rotate_angleZ, 0, 0, 1);
+  if (rotatingAntiClockwise) {
+    rotate_angleZ += rotate_angle_increment;
+    if (rotate_angleZ >= kRotateAngleLimit) {
+      rotate_angleZ = kDefaultRotateAngle;
+    }
+  }
+  if (rotatingClockwise) {
+    rotate_angleZ -= rotate_angle_increment;
+    if (rotate_angleZ <= -kRotateAngleLimit) {
+      rotate_angleZ = kDefaultRotateAngle;
+    }
+  }
+
+  glScalef(scaleX, scaleY, scaleZ);
+  if (scalingL) {
+    scaleX -= scale_increment;
+    if (scaleX < kScaleMin) {
+      scaleX = kScaleMin;
+    }
+  }
+  if (scalingR) {
+    scaleX += scale_increment;
+    if (scaleX > kScaleMax) {
+      scaleX = kScaleMax;
+    }
+  }
+  if (scalingU) {
+    scaleY += scale_increment;
+    if (scaleY > kScaleMax) {
+      scaleY = kScaleMax;
+    }
+  }
+  if (scalingD) {
+    scaleY -= scale_increment;
+    if (scaleY < kScaleMin) {
+      scaleY = kScaleMin;
+    }
+  }
+  if (scalingNear) {
+    scaleZ += scale_increment;
+    if (scaleZ > kScaleMax) {
+      scaleZ = kScaleMax;
+    }
+  }
+  if (scalingFar) {
+    scaleZ -= scale_increment;
+    if (scaleZ < kScaleMin) {
+      scaleZ = kScaleMin;
     }
   }
 
@@ -183,7 +355,9 @@ int main() {
     return EXIT_FAILURE;
   }
 
-  GLFWwindow *window = glfwCreateWindow(800, 800, "", NULL, NULL);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  GLFWwindow *window =
+      glfwCreateWindow(kWindowWidth, kWindowHeight, "", NULL, NULL);
   if (!window) {
     std::cerr << "Falha ao criar a janela GLFW" << std::endl;
     glfwTerminate();
